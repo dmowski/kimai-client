8a5541c947c85ad89f27c4ee5013252b073225a1
Dmowski Alexandr <dmowski@yandex.ru>
2020-02-10 19:47:11 +0300

    Move logic for display days

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { ReportContext } from "../context/ReportContext";
+import dayjs from "dayjs";
+import converter from "../converters";
 
 import "../styles/Reports.scss";
 // import ReportEditor from "./ReportEditor";
@@ -13,22 +15,71 @@ export default function Reports() {
   }, []);
 
   const reports = state.reports || [];
-  console.log("reports", reports);
+  const todayString = dayjs(Date()).format("YYYY-MM-DD");
+  const listOfDays = [
+    ...new Set(
+      reports.map(report => {
+        return converter.date.toView(report.begin);
+      })
+    )
+  ].sort((dayA, dayB) => {
+    var a = new Date(dayA).getTime();
+    var b = new Date(dayB).getTime();
+    if (a > b) {
+      return -1;
+    }
+    if (a < b) {
+      return 1;
+    }
+    return 0;
+  });
+
+  const daysBlock = listOfDays.map(day => {
+    const listOfReports = reports.filter(report => {
+      const reportDay = converter.date.toView(report.begin);
+      return reportDay === day;
+    });
+
+    const afterTitle = day === todayString ? " (Today)" : "";
+
+    return {
+      title: day + afterTitle,
+      reports: listOfReports
+    };
+  });
+
+  function getDuration(reports) {
+    const globalDuration = reports.reduce((duration, report) => {
+      return duration + report.duration;
+    }, 0);
+    return converter.duration.toView(globalDuration);
+  }
+
+  function getReports(reports) {
+    return reports.map(report => {
+      return <PreviewReport report={report} key={report.id} />;
+    });
+  }
+
+  function getDays() {
+    return daysBlock.map(dayBlock => {
+      return (
+        <div className="day-block" key={dayBlock.title}>
+          <p className="day-title">
+            <b>{dayBlock.title}</b>
+            <span>• {getDuration(dayBlock.reports)}</span>
+          </p>
+          {getReports(dayBlock.reports)}
+        </div>
+      );
+    });
+  }
 
   return (
     <div className="reports">
       <div className="list">
         <h3>Reports</h3>
-
-        <div className="day-block">
-          <p className="day-title">
-            <b>title</b>
-            <span>• 2h</span>
-          </p>
-          {reports.map(report => {
-            return <PreviewReport report={report} key={report.id} />;
-          })}
-        </div>
+        {getDays()}
       </div>
 
       <div className="report-editor">
