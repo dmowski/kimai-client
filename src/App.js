1037e1cb92bb17f4700d4b54459ab68403d0fded
Dmowski Alexandr <dmowski@yandex.ru>
2020-03-12 21:42:48 +0300

    Refactoring: split style for bottom-color to separate file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./styles/App.scss";
 import { ReportState } from "./context/report/ReportState";
 import { AuthState } from "./context/auth/AuthState";
 import { Main } from "./pages/Main";
