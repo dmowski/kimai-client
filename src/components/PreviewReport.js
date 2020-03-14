9fe536b9a5761f659fef156dcd3a84f5f139b212
Alexandr Dmowski <dmowski@yandex.ru>
2020-03-14 19:32:39 +0300

    Use static date for display preview report

--- src/components/PreviewReport.js
+++ src/components/PreviewReport.js
@@ -6,12 +6,22 @@ import { ReportContext } from "../context/report/ReportContext";
 const cssClass = "preview-report";
 
 export default function PreviewReport({ report = {} }) {
-  const { selectReport, selectedReport } = useContext(ReportContext);
+  const { selectReport, selectedReport, staticData } = useContext(
+    ReportContext
+  );
 
   const classList = [cssClass];
   if (selectedReport.id === report.id) {
     classList.push(`${cssClass}__selected`);
   }
+
+  const project = staticData.projects.find(
+    project => project.id === report.project.id
+  );
+
+  const activity = staticData.activities.find(
+    activity => activity.id === report.activity.id
+  );
 
   return (
     <article
@@ -21,8 +31,8 @@ export default function PreviewReport({ report = {} }) {
       }}
     >
       <p className={`${cssClass}__description`}>{report.description}</p>
-      <p className={`${cssClass}__project`}>{report.project?.name}</p>
-      <p className={`${cssClass}__activity`}>{report.activity?.name}</p>
+      <p className={`${cssClass}__project`}>{project?.name}</p>
+      <p className={`${cssClass}__activity`}>{activity?.name}</p>
       <p className={`${cssClass}__duration`}>
         {converters.duration.toView(report.duration)}
       </p>
